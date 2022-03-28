9e7a2d333073d3486237aedb92e649409be8a573
Kamil Chmurzynski <cloudjubei@gmail.com>
2022-03-29 00:33:11 +0200

    fix

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,12 @@
 
 import Image from 'next/image'
-import utilStyles from '../styles/utils.module.css'
 
 export default function Footer({ siteTitle }) {
     return (
-      <footer className={utilStyles.footer}>
+      <footer>
         <Image
           priority
           src="/images/app.png"
-          className={utilStyles.borderCircle}
           height={40}
           width={40}
           alt={siteTitle}
